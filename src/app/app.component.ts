397262206471f9a0ff7bf47326636db25210e167
Pedro Neto <piromaticos@gmail.com>
2023-09-28 20:36:32 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos'
+
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_portifolio';
+  items: MenuItem[] | undefined;
+
+  activeItem: MenuItem | undefined;
+
+  ngOnInit(){
+    AOS.init()
+    window.addEventListener('load',AOS.refresh)
+
+
+    this.items = [
+      { label: 'Início', icon: 'pi pi-fw pi-home' },
+      { label: 'Sobre mim', icon: 'pi pi-fw pi-calendar' },
+      { label: 'Projetos', icon: 'pi pi-fw pi-pencil' },
+      { label: 'Habilidades', icon: 'pi pi-fw pi-file' },
+      { label: 'Settings', icon: 'pi pi-fw pi-cog' }
+  ];
+
+  this.activeItem = this.items[0];
+  }
+  onActiveItemChange(event: MenuItem) {
+    this.activeItem = event;
 }
+
+activateLast() {
+    this.activeItem = (this.items as MenuItem[])[(this.items as MenuItem[]).length - 1];
+}
+
+}
+
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+
+import { MenuItem } from 'primeng/api';
+import { TabMenuModule } from 'primeng/tabmenu';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
